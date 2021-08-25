bb21a8a314ae23de6e03e10eca5e8a071267f023
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-25 17:20:46 +0600

    arrow function

--- recap-es6.js
+++ recap-es6.js
@@ -1,6 +1,6 @@
 const myBrather = "Delowr hossain";
 let myGF = "Tanisa";
- myGF = "Arita";
+myGF = "Arita";
 const myFriend = {
     name: "Ratul",
     age: 22,
@@ -13,4 +13,20 @@ I am Abu Daud Hossain. My Brathers name ${myBrather}.
 my gf name ${myGF}. My bestu name ${myFriend.name}. He is ${myFriend.age} old
 .`
 
-console.log(myStringTemp);
+// console.log(myStringTemp);
+
+const fiveMulti = num => num * 5;
+
+const twoParamiet = (num1, num2) => (num1 + 2) * (num2 + 2);
+const threeParamitar = (num1, num2, num3) => num1 * num2 * num3;
+const multiLineArrowfunction = (num1, num2) => {
+    const sum1 = num1 + 2;
+    const sum2 = num2 + 2;
+    const result = sum1 * sum2;
+    return result;
+}
+
+console.log(fiveMulti(5));
+console.log(twoParamiet(5, 3));
+console.log(threeParamitar(2, 3, 4));
+console.log(multiLineArrowfunction(3, 4))
