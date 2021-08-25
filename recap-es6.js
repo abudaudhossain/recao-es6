49afb89ac97496ceab46d8798491f60fe3e04d60
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-25 17:01:44 +0600

    add recap-es6

---
+++ recap-es6.js
@@ -0,0 +1,16 @@
+const myBrather = "Delowr hossain";
+let myGF = "Tanisa";
+ myGF = "Arita";
+const myFriend = {
+    name: "Ratul",
+    age: 22,
+    PaymentAddress: "Rowmari, Kurigram",
+    phone: "01783739283"
+}
+
+const myStringTemp = `hi,
+I am Abu Daud Hossain. My Brathers name ${myBrather}.
+my gf name ${myGF}. My bestu name ${myFriend.name}. He is ${myFriend.age} old
+.`
+
+console.log(myStringTemp);
